f7ad46dae3baead7ffc771e4750b04445342cbee
ahra2025 <abuhanzala2025@gmailc.com>
2023-07-03 10:52:24 +0530

    changes

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ComponentsModule } from './components/components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,11 @@ import { ComponentsModule } from './components/components.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
+    BrowserAnimationsModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dashboard/src/app/pages/login/login.component.ts
+++ dashboard/src/app/pages/login/login.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup = this.form.group({
+    userName: [null, Validators.required],
+    password: [null, Validators.required],
+  })
+  
+  constructor(
+    private form: FormBuilder, 
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  doLogin(){
+    if(this.loginForm.valid){
+      if(this.loginForm.value.userName == 'admin' && this.loginForm.value.password == 'Project@2023'){
+        this.toastr.info('Logged in', 'info', {timeOut: 2000});
+        this.router.navigateByUrl('dashboard');
+      }
+      else{
+        this.toastr.error('Bad Credentials', 'Error', {timeOut: 2000});
+      }
+    }
   }
 
 }
